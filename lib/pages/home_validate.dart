0a000377d2b0e6367c7dd203187df6d010a319a5
ChenPriew <suriya0957329813@gmail.com>
2024-08-09 22:01:09 +0700

    create ValidateLotto page

---
+++ lib/pages/home_validate.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class HomeValidate extends StatefulWidget {
+  const HomeValidate({super.key});
+
+  @override
+  State<HomeValidate> createState() => _HomeValidateState();
+}
+
+class _HomeValidateState extends State<HomeValidate> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(),
+    );
+  }
+}
