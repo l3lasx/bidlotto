ab7078f3bfce5de11eeec6f59124c7842572a0b3
ChenPriew <suriya0957329813@gmail.com>
2024-08-11 18:50:20 +0700

    change font

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:bidlotto/pages/login.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,9 +12,12 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      theme: ThemeData(
+          textTheme: GoogleFonts.promptTextTheme(),
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.red)),
       title: 'Bidlotto',
-      home: LoginPage(),
+      home: const LoginPage(),
     );
   }
 }
